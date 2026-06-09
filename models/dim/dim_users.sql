71f8f26e889fd38b4c839564b1e61abc156053d1
Anuj Yadav <anujyadav@Anujs-MacBook-Pro.local>
2026-06-09 21:28:05 +0530

    Added dim-genome, users

---
+++ models/dim/dim_users.sql
@@ -0,0 +1,13 @@
+WITH rating AS(
+    SELECT DISTINCT user_Id FROM {{ ref('src_ratings')}}
+),
+tags AS(
+    SELECT DISTINCT user_id FROM {{ ref('src_tags')}}
+)
+
+SELECT DISTINCT user_id
+FROM (
+    SELECT * FROM rating
+    UNION
+    SELECT * FROM tags
+)
